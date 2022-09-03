c135130a056dea0b214a8927dc7734ffd709bd9d
Chaima Rouzzi <ic_rouzzi@esi.dz>
2022-09-03 15:34:14 +0100

    first-commit

---
+++ src/compoments/ChannelSerch.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChannelSerch = () => {
+  return <div>ChannelSerch</div>;
+};
+
+export default ChannelSerch;
---
+++ src/compoments/TeamChannelList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TeamChannelList = () => {
+  return <div>TeamChannelList</div>;
+};
+
+export default TeamChannelList;
