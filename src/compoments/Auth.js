60e4f766fda0790010e2553c85c5a7469bcfdfd7
Chaima Rouzzi <ic_rouzzi@esi.dz>
2022-09-05 13:02:17 +0100

    URL fixing

--- src/compoments/Auth.js
+++ src/compoments/Auth.js
@@ -29,7 +29,7 @@ const Auth = () => {
     e.preventDefault();
 
     const { username, password, phoneNumber, avatarURL } = form;
-    const URL = "http://localhost:5000";
+    const URL = "https://chat-withe-chaima.herokuapp.com";
     const {
       data: { token, userId, hashedPassword, fullName },
     } = await axios.post(`${URL}/auth/${isSignup ? `signup` : `signin`}`, {
